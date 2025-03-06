a5aeddd1d119a764c15d75a4b7fab2de24f6b58f
303 - Programacion <303@laboratorios.usbbog.edu.co>
2025-03-05 21:08:20 -0500

    Mensaje de tu commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(MyApp());
